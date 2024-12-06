f6debfd8ef66d22fac4e716cd7379b7432721668
474 <a@qwa.su>
2024-12-06 12:16:40 +0500

    Add sidebar support for mobile devices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 const burger = document.getElementById('qtk_burger');
 const menu = document.getElementById('qtk_menu');
+const sidebar_switcher = document.getElementById('qtk_sidebar_switcher');
+const aside = document.getElementById('qtk_aside');
 const root = document.documentElement;
 
 burger.addEventListener('click', () => {
@@ -7,5 +9,13 @@ burger.addEventListener('click', () => {
   burger.setAttribute('aria-expanded', !expanded);
   root.classList.toggle('burger-active');
   menu.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
+
+sidebar_switcher.addEventListener('click', () => {
+  const expanded = sidebar_switcher.getAttribute('aria-expanded') === 'true';
+  sidebar_switcher.setAttribute('aria-expanded', !expanded);
+  root.classList.toggle('aside-active');
+  aside.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
 });
